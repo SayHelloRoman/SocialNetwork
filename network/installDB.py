673cacecf3aec1ccb78ded16f7a0920d9cd7edca
Roman <68429513+SayHelloRoman@users.noreply.github.com>
2021-03-08 15:12:48 +0200

    Add files via upload

---
+++ network/installDB.py
@@ -0,0 +1,22 @@
+import psycopg2
+import json
+
+
+with open("data.json") as file:
+	db = psycopg2.connect(
+		**json.load(file)
+	)
+
+cursor = db.cursor()
+cursor.execute("""
+CREATE TABLE users
+(
+    Id VARCHAR(20) NOT NULL PRIMARY KEY,
+    status TEXT,
+    pasword VARCHAR(20)
+);
+""")
+
+cursor.close()
+db.commit()
+db.close()
